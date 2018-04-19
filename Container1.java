14da2af93af3ffe5ed94da2de7a872689b236b70
ajenglan <38438593+ajenglan@users.noreply.github.com>
2018-04-18 20:05:00 -0700

    Update Container1.java
    Notes are in the file

--- Container1.java
+++ Container1.java
@@ -15,27 +15,42 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /*
- * Updates:
- *  -fixed left/right justify (was broken after fullJustify implementation)
- *  -Full justify works closer to how it should
+ * Notes (A.J. England) 4-18-18 @ 8pm:
+ *  Updates:
+ *  	 -all the justify methods should be completely functional now
+ *  	 -no longer a blank space after lines or newlines after last line
+ *  	 -some changes to Container3.java too
+ *  Issues and things to add:
+ *  	 -remove the output preview page and go straight to analysis page
+ * 		 -add the new lines to the analysis page
+ * 		 -add double spacing option
+ * 		 -add line length option
+ * 		 -make the "restart" button do something? Or maybe remove it
  *  
- * Issues and things to add:
- *  -remove the output preview page and go straight to analysis page
- *  -add the new lines to the analysis page
- *  -full justify works for some cases, but in situations where more than one space needs to be added between words it fails
- *  -add double spacing option
- *  -add line length option
+ * Notes (A.J. England) 4-18-18 @ 2pm:
+ * 	Updates:
+ *  	 -fixed left/right justify (was broken after fullJustify implementation)
+ *  	 -Full justify works closer to how it should
+ *	Issues and things to add:
+ * 		 -remove the output preview page and go straight to analysis page
+ * 		 -add the new lines to the analysis page
+ * 		 -full justify works for some cases, but in situations where more than one space needs to be added between words it fails
+ * 		 -add double spacing option
+ * 		 -add line length option
+ * 		 -for right/left justify the output lines are length 81, not 80
+ * 			-and full justify is 82 (81 + 1 space after last word on each line)
+ * 
  */
 
-
-@SuppressWarnings({ "serial", "unused", "hiding" })
+@SuppressWarnings(
+{ "serial", "unused", "hiding" })
 public class Container1 extends JPanel
 {
 	int lineLength = 80;
 	File inputFile;
 	File outputFile;
 	JFrame jframe;
-	
+
 	public Container1(JFrame jframe)
 	{
 		lineLength = 80;
@@ -44,7 +59,7 @@ public class Container1 extends JPanel
 		JPanel input = new JPanel();
 		FlowLayout inputLayout = new FlowLayout();
 		input.setLayout(inputLayout);
-		
+
 		JLabel label1 = new JLabel("Input: ");
 		JTextField fileName = new JTextField("", 20);
 		fileName.setEditable(false);
@@ -67,23 +82,23 @@ public class Container1 extends JPanel
 				}
 			}
 		});
-		
+
 		input.add(label1);
 		input.add(fileName);
 		input.add(chooseFile);
-		
+
 		// output section
 		JPanel output = new JPanel();
 		FlowLayout outputLayout = new FlowLayout();
 		output.setLayout(outputLayout);
-		
+
 		JLabel label2 = new JLabel("Output: ");
 		JTextField outputFileName = new JTextField("", 20);
 		outputFileName.setEditable(false);
 		JButton chooseOutputFile = new JButton("...");
 		chooseOutputFile.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
@@ -100,15 +115,15 @@ public class Container1 extends JPanel
 				}
 			}
 		});
-		
+
 		output.add(label2);
 		output.add(outputFileName);
 		output.add(chooseOutputFile);
-		
+
 		// justify section
 		JPanel justify = new JPanel();
 		justify.setLayout(outputLayout);
-		
+
 		JLabel label3 = new JLabel("Justify: ");
 		JRadioButton leftJustify = new JRadioButton("Left");
 		JRadioButton fullJustify = new JRadioButton("Full");
@@ -117,17 +132,17 @@ public class Container1 extends JPanel
 		bG.add(leftJustify);
 		bG.add(fullJustify);
 		bG.add(rightJustify);
-		
+
 		justify.add(label3);
 		justify.add(leftJustify);
 		justify.add(rightJustify);
 		justify.add(fullJustify);
-		
+
 		// Okay button
 		JButton okay = new JButton("Okay");
 		okay.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
@@ -143,42 +158,74 @@ public class Container1 extends JPanel
 					// @SuppressWarnings("resource")
 					if (!(leftJustify.isSelected()))
 					{
-						// System.out.println("Is this running?");
+						// array of Strings, each string is a line of the left-justified output
 						String[] lines = formattedOutput.split("\n");
-						
-						if (fullJustify.isSelected()) 
+
+						// A.J. England notes:
+						// new full-justify method, completely functional in my (very limited) testing
+						if (fullJustify.isSelected())
 						{
 							formattedOutput = "";
+
+							// for each line i
 							for (int i = 0; i < lines.length; i++)
 							{
 								int whiteSpaceToAdd = lineLength - lines[i].length();
+								// array of strings, each string is a word in the line of lines[i]
 								String[] words = lines[i].split(" ");
 								int whiteSpaceAdded = 0;
+
+								// this is used when more than one extra space needs to be added between words
+								// to justify correctly
+								int mult = 0; // how many non-space characters are on line i
+								for (int j = 0; j < words.length; j++)
+									mult += words[j].length();
+								mult = (whiteSpaceToAdd / (words.length - 1)) + 1;
+								System.out.print(whiteSpaceToAdd + " ");
+								whiteSpaceToAdd = whiteSpaceToAdd - ((mult - 1) * (words.length - 1));
+								System.out.println(whiteSpaceToAdd + " " + words.length + " " + mult);
+								// for each word j on lines[i]
 								for (int j = 0; j < words.length; j++)
 								{
-									formattedOutput += words[j] + " ";
-									if (whiteSpaceAdded < whiteSpaceToAdd)
+									formattedOutput += words[j];
+									if (j + 1 < words.length)
 									{
+										for (int k = 0; k < mult; k++)
+										{
+											formattedOutput += " ";
+										}
+										if (whiteSpaceAdded < whiteSpaceToAdd)
+										{
 											formattedOutput += " ";
 											whiteSpaceAdded++;
+										}
 									}
+
 								}
 								formattedOutput += "\n";
 							}
-							
-							
 						}
-						
-						
-						
-						
-						if (rightJustify.isSelected()) 
+
+						// old fullJustify formatter, new one is above
+						/*
+						 * if (fullJustify.isSelected()) { formattedOutput = ""; for (int i = 0; i <
+						 * lines.length; i++) { int whiteSpaceToAdd = lineLength - lines[i].length();
+						 * String[] words = lines[i].split(" "); int whiteSpaceAdded = 0; for (int j =
+						 * 0; j < words.length; j++) { formattedOutput += words[j] + " "; if
+						 * (whiteSpaceAdded < whiteSpaceToAdd) { formattedOutput += " ";
+						 * whiteSpaceAdded++; } } formattedOutput += "\n"; }
+						 * 
+						 * }
+						 */
+
+						if (rightJustify.isSelected())
 						{
 							formattedOutput = "";
 							for (int i = 0; i < lines.length; i++)
 							{
 								String holder = lines[i];
 								String modified = "";
+								System.out.println(i + " " + lines[i].length());
 								for (int j = lines[i].length(); j < lineLength; j++)
 								{
 									modified += " ";
@@ -186,34 +233,35 @@ public class Container1 extends JPanel
 								modified += holder + "\n";
 								formattedOutput += modified;
 							}
-						
+
 						}
-						
+						if (formattedOutput.charAt(formattedOutput.length() - 1) == '\n')
+							formattedOutput = formattedOutput.substring(0, formattedOutput.length() - 1);
 					}
 					PrintWriter out = new PrintWriter(outputFile);
 					/* String */ newFormattedOutput = formattedOutput.replaceAll("\n", System.lineSeparator());
 					out.write(newFormattedOutput);
 					out.close();
-				} 
+				}
 				catch (FileNotFoundException ex)
 				{
 					System.out.println("File not found.");
-				} 
+				}
 				catch (IOException ex)
 				{
 					System.out.println("Could not write file.");
-				} 
+				}
 				catch (NullPointerException ex)
 				{
-					
+
 				}
-				
+
 				JPanel container2 = new Container2(jframe, newFormattedOutput, inputFile);
 				jframe.add(container2);
 				jframe.revalidate();
 				jframe.repaint();
 			}
-			
+
 		});
 		// container panel
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -222,28 +270,28 @@ public class Container1 extends JPanel
 		this.add(justify);
 		this.add(okay);
 	}
-	
+
 	public File getInputFile()
 	{
 		return inputFile;
 	}
-	
+
 	public File getoutputFile()
 	{
 		return outputFile;
 	}
-	
+
 	public JFrame getJFrame()
 	{
 		return jframe;
 	}
-	
+
 	private String parse(File file, boolean fullJust)
 	{
-		
+
 		String line = null;
 		String formattedOutput = "";
-		
+
 		try
 		{
 			FileReader fileReader = new FileReader(file);
@@ -252,65 +300,76 @@ public class Container1 extends JPanel
 			ArrayList<String> words = new ArrayList<String>();
 			while ((line = bufferedReader.readLine()) != null)
 			{
-				
+
 				if (line.isEmpty())
 				{
-					
-				} 
+
+				}
 				else
 				{
-					
+
 					String[] lineOfWords = line.split("\\s+");
-					
+
 					for (int i = 0; i < lineOfWords.length; i++)
 					{
 						words.add(lineOfWords[i]);
 					}
-					
+
 					/*
-					 * for (int i = 0; i < line.length(); i++) { if
-					 * (line.charAt(i) != ' ') { word += line.charAt(i); } else
-					 * { words.add(word); word = ""; } }
+					 * for (int i = 0; i < line.length(); i++) { if (line.charAt(i) != ' ') { word
+					 * += line.charAt(i); } else { words.add(word); word = ""; } }
 					 */
 				}
 			}
-			
+
 			int chars = 0;
 			String holder = "";
-			
+
 			// iterate through words
 			for (int i = 0; i < words.size(); i++)
 			{
-					
-				if (chars == 0 && words.get(i).length() > lineLength)
+
+				if (chars == 0 && words.get(i).length() >= lineLength)
 				{
 					formattedOutput += words.get(i) + "\n";
-				} 
+				}
 				else
 				{
 					chars += words.get(i).length();
-					
-					if (chars > lineLength)
+
+					if (chars >= lineLength)
 					{
+						if (chars > lineLength)
+						{
+							chars -= words.get(i).length();
+							holder = holder.substring(0, holder.length() - 1);
+							holder += "\n";
+
+							formattedOutput += holder;
 							
-						chars -= words.get(i).length();
-						holder = holder.substring(0, holder.length() - 1);
-						holder += "\n";
-							
-						formattedOutput += holder;
-							
-						holder = "";
-						chars = 0;
-						i--;
-					} 
-					else if (chars <= lineLength)
+							holder = "";
+							chars = 0;
+							i--;
+						}
+						else if (chars == lineLength)
+						{
+							holder += words.get(i);
+							holder += "\n";
+							formattedOutput += holder;
+							holder = "";
+							chars = 0;
+						}
+						if (formattedOutput.charAt(formattedOutput.length() - 2) == ' ')
+							formattedOutput = formattedOutput.substring(0, formattedOutput.length() - 2) + "\n";
+					}
+					else if (chars < lineLength)
 					{
 						holder += words.get(i) + " ";
 						chars += 1;
 					}
 				}
-			
-				//formattedOutput += holder + "\n";
+
+				// formattedOutput += holder + "\n";
 			}
 			formattedOutput += holder + "\n";
 		}
@@ -318,18 +377,21 @@ public class Container1 extends JPanel
 		{
 			System.out.println("Unable to open file.");
 		}
-		
+
 		catch (IOException ex)
 		{
 			System.out.println("Error reading file.");
-		} catch (NullPointerException ex)
+		}
+		catch (NullPointerException ex)
 		{
 			System.out.println("Error reading file.");
 		}
-		
+
 		System.out.println(formattedOutput);
+		if (formattedOutput.charAt(formattedOutput.length() - 2) == ' ')
+			formattedOutput = formattedOutput.substring(0, formattedOutput.length() - 2);
 		return formattedOutput;
-		
+
 	}
-	
+
 }
